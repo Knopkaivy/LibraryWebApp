655bced8ce3289a87737f93aa5b0f83e9163e78f
TIVY-PK\tivy <tetiana.ivy@concentrix.com>
2024-09-08 20:36:55 -0500

    Add project files.

---
+++ LibraryWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using LibraryWebApp.Models;
+
+namespace LibraryWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<LibraryWebApp.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ LibraryWebApp/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryWebApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime? ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Cover Image URL")]
+        public string? CoverImageUrl { get; set; }
+    }
+}
