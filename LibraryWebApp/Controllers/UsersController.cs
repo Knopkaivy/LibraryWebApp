543082125b1a760fcf094c6c589b79007122794e
TIVY-PK\tivy <tetiana.ivy@concentrix.com>
2024-12-15 11:00:37 -0600

    Add edit user role functionality

--- LibraryWebApp/Controllers/UsersController.cs
+++ LibraryWebApp/Controllers/UsersController.cs
@@ -16,20 +16,33 @@ namespace LibraryWebApp.Controllers
     {
         private readonly ApplicationDbContext _context;
         private UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
         private IPasswordHasher<ApplicationUser> _passwordHasher;
 
-        public UsersController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, IPasswordHasher<ApplicationUser> passwordHasher)
+        public UsersController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IPasswordHasher<ApplicationUser> passwordHasher)
         {
             _context = context;
             _userManager = userManager;
+            _roleManager = roleManager;
             _passwordHasher = passwordHasher;
         }
 
         // GET: User
         public IActionResult Index()
         {
-
-            return View(_userManager.Users);
+            var users = _userManager.Users;
+            List<UserViewModel> userList = new List<UserViewModel>();
+            foreach(var user in users)
+            {
+                UserViewModel userViewModel = new UserViewModel
+                {
+                    User = user,
+                    UserId = user.Id,
+                    Roles = [.. _userManager.GetRolesAsync(user).Result],
+                };
+                userList.Add(userViewModel);
+            }
+            return View(userList);
         }
 
         // GET: Users/Details/5
@@ -119,6 +132,76 @@ namespace LibraryWebApp.Controllers
                     user.DateOfBirth = DateOfBirth;
 
                     await _userManager.UpdateAsync(user);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApplicationUserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_userManager.Users, "Id", "Id", user.Id);
+            return View(user);
+        }
+
+        // GET: Users/EditRole/5
+        public async Task<IActionResult> EditRole(string? id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            UserViewModel userViewModel = new UserViewModel
+            {
+                User = user,
+                UserId = user.Id,
+                Roles = [.. _userManager.GetRolesAsync(user).Result],
+            };
+
+            return View(userViewModel);
+        }
+
+        // POST: Users/EditRole/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditRole(string id, List<string> RoleList)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var roles = _roleManager.Roles.ToList();
+            List<string> userRoles = [.. _userManager.GetRolesAsync(user).Result];
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    foreach (var role in roles)
+                    {
+                        if(!userRoles.Contains(role.Name) && RoleList.Contains(role.Name))
+                        {
+                            await _userManager.AddToRoleAsync(user, role.Name);
+                        } else if(userRoles.Contains(role.Name) && !RoleList.Contains(role.Name))
+                        {
+                            await _userManager.RemoveFromRoleAsync(user, role.Name);
+                        }
+                    }
                 }
                 catch (DbUpdateConcurrencyException)
                 {
--- LibraryWebApp/Models/UserViewModel.cs
+++ LibraryWebApp/Models/UserViewModel.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public ApplicationUser User { get; set; }
         public string UserId { get; set; }
+        public List<string> Roles { get; set; }
     }
 }
